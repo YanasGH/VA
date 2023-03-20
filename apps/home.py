e52fb1294e746a65b2317b45b3190636b8035621
YanasGH <yana.stoyanova2000@gmail.com>
2023-03-20 11:10:30 +0100

    Create home.py

---
+++ apps/home.py
@@ -0,0 +1,53 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+# needed only if running this as a single page app
+#external_stylesheets = [dbc.themes.LUX]
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# change to app.layout if running as single page app instead
+layout = html.Div([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1("Welcome to the GAStech Disappearance Visual Analytics tool of group 3", className="text-center")
+                    , className="mb-5 mt-5")
+        ]),
+        dbc.Row([
+            dbc.Col(html.H5(children='You can find the complete data of the GAStech Disappearance Challenge by clicking at the link below.'
+                                     )
+                    , className="mb-4")
+            ]),
+
+        dbc.Row([
+            dbc.Col(html.H5(children='The tool consists of 2 main pages: Home tab, which is an introduction page to the visual analytics tool. Exploration tab about the email correspondence between employees, which gives the oppurtunity to find interesting patterns.')
+                    , className="mb-5")
+        ]),
+
+        dbc.Row([
+            dbc.Col(dbc.Card(children=[html.H3(children='Get the complete data used in this project',
+                                               className="text-center"),
+                                       dbc.Button("GAStech Disappearance",
+                                                  href="https://openscholarship.wustl.edu/cgi/viewcontent.cgi?filename=1&article=2189&context=cse_research&type=additional",
+                                                  color="primary",
+                                                  target="_blank",
+                                                  className="mt-3")
+                                       ],
+                             body=True, color="dark", outline=True)
+                    , width=6, className="mb-4"),
+
+            dbc.Col(dbc.Card(children=[html.H3(children='You can find the code for this project in',
+                                               className="text-center"),
+                                       dbc.Button("GitHub",
+                                                  href="https://github.com/ddonov1905/visualisation_app/",
+                                                  color="primary",
+                                                  target="_blank",
+                                                  className="mt-3"),
+                                       ],
+                             body=True, color="dark", outline=True)
+                    , width=6, className="mb-4")
+        ], className="mb-5")
+
+    ])
+
+])
