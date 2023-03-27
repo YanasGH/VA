f0035e03da9a56aaf4bc2ac230eb2d0fb976afac
YanasGH <yana.stoyanova2000@gmail.com>
2023-03-27 11:19:18 +0200

    Update

--- department_chord_graph_main.py
+++ department_chord_graph_main.py
@@ -1,7 +1,5 @@
 from collections import Counter
 import plotly.graph_objects as go
-from nltk.sentiment import SentimentIntensityAnalyzer
-from itertools import product
 import holoviews as hv
 from holoviews import opts, dim
 from bokeh.models import HoverTool
@@ -16,28 +14,17 @@ warnings.filterwarnings("ignore")
 
 print('----- Creating and saving 10 graphs -----')
 
-sia = SentimentIntensityAnalyzer()
-
 email_df = pd.read_csv('email_headers.csv', encoding='cp1252')
-sentiment = email_df['Subject'].apply(lambda x: sia.polarity_scores(x))
-email_df[['neg', 'neu', 'pos', 'compound']] = pd.json_normalize(sentiment)
 employee_recs = pd.read_excel("EmployeeRecords.xlsx")
+cmap_custom = ['#5b4dd6', '#c933bc', '#ffc60a', '#ff5960', '#ff9232', '#ff2b90']
 
 email_df['Day'] = None
 email_df['DepTo'] = None
 email_df['DepFrom'] = None
-email_df['sentiment'] = None
 emp_dict = dict(zip(employee_recs.EmailAddress, employee_recs.CurrentEmploymentType))
 for i in range(len(email_df)):
     email_df['Day'][i] = str(dt.strptime(email_df['Date'][i], "%m/%d/%Y %H:%M").day)
     email_df['DepFrom'][i]=emp_dict.get(email_df['From'][i])
-    
-    if email_df['compound'][i]==0:
-        email_df['sentiment'][i]='neu'
-    elif email_df['compound'][i]>0:
-        email_df['sentiment'][i]='pos'
-    else:
-        email_df['sentiment'][i]='neg'
     
     to_list = []
     for to in email_df['To'][i].split(', '):
@@ -95,58 +82,33 @@ _, edge2, node2 = edge_node('EmailAddress', 'To', 'From')
 
 name_email_dict = dict(zip(node2['Account'].to_list(), node2['CustomerName'].to_list()))
 emp_dep_dict = dict(zip(employee_recs['EmailAddress'].to_list(), employee_recs['CurrentEmploymentType'].to_list()))
-
 df_chord_nodes = employee_recs[['EmailAddress']]
 df_chord_nodes['Department'] = None
 df_chord_nodes['Name'] = None
+
 for i in range(len(df_chord_nodes)):
     df_chord_nodes['Department'][i] = emp_dep_dict.get(df_chord_nodes['EmailAddress'][i])
     df_chord_nodes['Name'][i] = name_email_dict.get(df_chord_nodes['EmailAddress'][i])   
-dict_nodes_index = dict(zip(employee_recs['EmailAddress'], df_chord_nodes.index))
 
-chord_sentiment_pre = pd.DataFrame(product(employee_recs['EmailAddress'], employee_recs['EmailAddress'], ['neg', 'pos', 'neu']), columns = ['source', 'target', 'sentiment'])#.head(4)
-chord_sentiment_pre = chord_sentiment_pre[chord_sentiment_pre['source']!=chord_sentiment_pre['target']].reset_index(drop=True)
-chord_sentiment_pre['value'] = np.zeros(len(chord_sentiment_pre))
-chord_sentiment_pre['dep'] = None
+_, df_chord_edges, _ = edge_node('EmailAddress', 'To', 'From')
+df_chord_edges = df_chord_edges.rename(columns={"TransactionAmt": "value"})
+df_chord_edges['source'] = None
+df_chord_edges['target'] = None
+df_chord_edges['Gsource'] = None
+df_chord_edges['Gtarget'] = None
 
-def sentiment_chord_graph(YEAR, chord_sentiment_pre):
-    
-    chord_sentiment = copy.deepcopy(chord_sentiment_pre)
-    for i in range(len(chord_sentiment)):
-        email_df_days = email_df[email_df['Day']==str(YEAR)]
-        email_df_filtered = email_df_days[email_df_days['From']==chord_sentiment['source'][i]]
+for i in range(len(df_chord_edges)):
+    df_chord_edges['source'][i] = int(df_chord_nodes[df_chord_nodes['EmailAddress']==df_chord_edges['Source'][i]].index[0])
+    df_chord_edges['target'][i] = int(df_chord_nodes[df_chord_nodes['EmailAddress']==df_chord_edges['Target'][i]].index[0])
+    df_chord_edges['Gsource'][i] = emp_dep_dict.get(df_chord_edges['Source'][i])
+    df_chord_edges['Gtarget'][i] = emp_dep_dict.get(df_chord_edges['Target'][i])
 
-        for index in email_df_filtered.index:
-            if chord_sentiment['target'][i] in email_df_filtered['To'][index]:
-                if email_df_filtered['sentiment'][index]==chord_sentiment['sentiment'][i]:
-                    chord_sentiment['value'][i]+=1
+df_chord_nodes = df_chord_nodes.drop(columns=['EmailAddress'])
+df_chord_edges = df_chord_edges.drop(columns = ['Source', 'Target'])
+df_chord_edges = df_chord_edges[['source', 'target', 'value', 'Gsource','Gtarget', 'Date']]
 
-        chord_sentiment['dep'][i] = emp_dep_dict[chord_sentiment['source'][i]]
-        chord_sentiment['source'][i] = dict_nodes_index[chord_sentiment['source'][i]]
-        chord_sentiment['target'][i] = dict_nodes_index[chord_sentiment['target'][i]]
-
-    chord_sentiment['value'] = chord_sentiment['value'].astype(int)
-    chord_sentiment = chord_sentiment[chord_sentiment['value']!=0]
-
-    df_count_sentiment = pd.DataFrame(chord_sentiment.groupby(by=['dep', 'sentiment'])['value'].sum()).reset_index().sort_values('value', ascending=False).drop_duplicates('dep')
-    dict_count_sentiment = dict(zip(df_count_sentiment['dep'].tolist(), df_count_sentiment['sentiment'].tolist()))
-
-    df_chord_nodes['sentimcolor'] = None
-    for i in range(len(df_chord_nodes)):
-        df_chord_nodes['sentimcolor'][i] = dict_count_sentiment[df_chord_nodes['Department'][i]]
-
-    chord_sentiment = chord_sentiment[['source','target','sentiment','value']]
-    dict_sentim_colors = {'pos': '#379475', 'neg': '#B51E17', 'neu': '#BDC4C5'}
-
-
-    cmap_custom_e = []
-    for s in chord_sentiment['sentiment'].unique():
-        cmap_custom_e.append(dict_sentim_colors[s])
-
-    cmap_custom_n = []
-    for s in df_chord_nodes['sentimcolor'].unique():
-        cmap_custom_n.append(dict_sentim_colors[s])
-
+def chord_graph(YEAR):
+    df_chord_edges_filtered = df_chord_edges[df_chord_edges['Date']==str(YEAR)].drop(columns = ['Date']) #using the df outside the function
     tooltips = [('Department', '@Department'), ('Name', '@Name')]
     hover = HoverTool(tooltips=tooltips)
     hv.extension('bokeh')
@@ -154,17 +116,17 @@ def sentiment_chord_graph(YEAR, chord_sentiment_pre):
 
     df_chord_nodes_filtered = hv.Dataset(df_chord_nodes, 'index')
 
-    chord = hv.Chord((chord_sentiment, df_chord_nodes_filtered))
-    chord.opts(opts.Chord(inspection_policy='nodes', cmap=cmap_custom_n, edge_cmap= cmap_custom_e,
-                                 edge_color=dim('sentiment').str(), labels='Name', node_color=dim('sentimcolor').str(),
+    chord = hv.Chord((df_chord_edges_filtered, df_chord_nodes_filtered))
+    chord.opts(opts.Chord(inspection_policy='nodes', cmap=cmap_custom, edge_cmap=cmap_custom,
+                                 edge_color=dim('Gsource').str(), labels='Name', node_color=dim('Department').str(),
                                  symmetric=True, bgcolor = '#26232C', label_text_color='#FEFEFE', tools = [hover]))
     return chord
 
 c = 1
 for YEAR in [6,7,8,9,10,13,14,15,16,17]:
-    chord_s = sentiment_chord_graph(YEAR, chord_sentiment_pre)
+    chord_g = chord_graph(YEAR)
     renderer = hv.renderer('bokeh')
     renderer.theme = Theme('assets/theme_chord.json')
-    renderer.save(chord_s, f'assets/graph_chord_sentiment_{YEAR}')
+    renderer.save(chord_g, f'assets/graph_chord_{YEAR}')
     print(f'----- {10-c} graphs left -----')
     c+=1
